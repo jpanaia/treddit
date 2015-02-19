548b7482fcc83f564a3536efbb794ce516576dc6
Jaime <jpanaia@gmail.com>
2015-02-18 19:46:18 -0500

    added vote model and set up associations

---
+++ app/views/votes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @vote, :id, :post_id, :user_id, :score, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218222708) do
+ActiveRecord::Schema.define(version: 20150219004025) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -40,5 +40,13 @@ ActiveRecord::Schema.define(version: 20150218222708) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
